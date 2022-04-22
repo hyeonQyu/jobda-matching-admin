7c46c3b996ab9a70f7a7e1ef52a96fe9264a5123
khg0408 <khg0408@midasin.com>
2022-04-22 17:39:57 +0900

    chore: 클라이언트 프로젝트 생성

---
+++ client/webpack.config.ts/common.ts
@@ -0,0 +1,82 @@
+const webpack = require('webpack');
+const path = require('path');
+const { TsconfigPathsPlugin } = require('tsconfig-paths-webpack-plugin');
+// const dotenv = require('dotenv');
+// const loadEnv = require('./env/loadEnv');
+
+module.exports = (mode) => {
+    // dotenv.config({
+    //     path: path.resolve(__dirname, `./env/${mode}.env`),
+    // });
+
+    return {
+        entry: './src/index.tsx',
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|js)x?$/i,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+                        },
+                    },
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: 'style-loader',
+                        }, // creates style nodes from JS strings
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: {
+                                    mode: 'local',
+                                    localIdentName: '[name]__[local]--[hash:base64:5]',
+                                },
+                                sourceMap: true,
+                            },
+                        }, // translates CSS into CommonJS
+                        {
+                            loader: 'resolve-url-loader',
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true,
+                            },
+                        }, // compiles Sass to CSS, using Node Sass by default
+                    ],
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.(png|jpg|gif|svg)$/,
+                    use: 'file-loader',
+                },
+                {
+                    test: /\.txt$/,
+                    use: 'raw-loader',
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+            plugins: [
+                new TsconfigPathsPlugin({
+                    configFile: path.resolve(__dirname, '../tsconfig.json'),
+                }),
+            ],
+        },
+        plugins: [
+            new webpack.ProvidePlugin({
+                process: 'process/browser',
+            }),
+            // new webpack.DefinePlugin({
+            //     'process.env': loadEnv(process.env),
+            // }),
+        ],
+        devtool: 'inline-source-map',
+    };
+};
