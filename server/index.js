1068b314b2587c36fc639a1d485d00bf728d61c3
Hyeongyu Kim <dhk0561@naver.com>
2022-04-24 02:10:29 +0900

    feat: companyList, jobGroupList 정보 불러오기 API 추가

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 
 const controller = require('./src/controller');
-for (const mapping in controller) {
-    controller[mapping](app);
+for (const path in controller) {
+    controller[path](app, path);
 }
+
+require('./src/util/jsonUtil').getJsonData('jobGroupList');
 
 app.listen(5000, () => {
     console.log('express server started with port 5000');
--- server/src/controller.js
+++ server/src/controller.js
@@ -1,11 +1,30 @@
+const { getJsonData } = require('./util/jsonUtil');
+
+const doCommonResponse = (app, path, callback) => {
+    app.get(`/api/${path}`, async (req, res) => {
+        console.log(`request: ${path}`);
+        try {
+            callback(req, res);
+            console.log('response 성공');
+        } catch (e) {
+            console.error(e);
+        }
+    });
+};
+
 /**
  * Controller
  */
 module.exports = {
-    getTest: (app) => {
-        app.get('/test-api', async (req, res) => {
-            console.log(req);
-            res.send(req);
+    getCompanyList: (app, path) => {
+        doCommonResponse(app, path, (req, res) => {
+            res.send(getJsonData('companyList'));
+        });
+    },
+
+    getJobGroupList: (app, path) => {
+        doCommonResponse(app, path, (req, res) => {
+            res.send(getJsonData('jobGroupList'));
         });
     },
 };
